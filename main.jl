28fe869dfab13f85d4a40b02731a2a15ccd41675
Jake Rosoman <jkroso@gmail.com>
2016-01-05 14:23:10 +1300

    fix deprecations

--- main.jl
+++ main.jl
@@ -78,12 +78,12 @@ end
 # Handle pretty printing for the REPL etc..
 #
 Base.writemime(io::IO, ::MIME"text/plain", r::Result) = begin
-  write(io, "$(r.pass ? '✓' : '✗') $(int(r.time * 1000))ms")
+  write(io, "$(r.pass ? '✓' : '✗') $(round(Int, r.time * 1000))ms")
 end
 
 Base.writemime(io::IO, ::MIME"text/html", r::Result) = begin
   css = "padding:1px 5px;font-size:14px;color:$(r.pass ? "rgb(0, 226, 0)" : "red");"
-  text = "$(r.pass ? '✓' : '✗') $(int(r.time * 1000))ms"
+  text = "$(r.pass ? '✓' : '✗') $(round(Int, r.time * 1000))ms"
   write(io, "<div style=\"$css\">$text</div>")
 end
 
