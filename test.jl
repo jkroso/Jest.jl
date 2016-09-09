01da9369b20a5f1da9ecc674edc433e2ef7e0474
Jake Rosoman <jkroso@gmail.com>
2016-09-10 09:31:43 +1200

    update test.jl

--- test.jl
+++ test.jl
@@ -1,11 +1,8 @@
-##
-# Normally the tests would be embedded with the code it tests
-# but in this case they need to be seperate so they don't get
-# run every time Jest is used
-#
-test("suites should group") do
+a = 1
+
+testset("suites should group") do
   @test a == 1
-  test("and should themselves be nestable") do
+  testset("and should themselves be nestable") do
     @test 2 == 2
   end
   @test 3 == 3
@@ -14,13 +11,12 @@ end
 
 @test a == 1
 
-test("failures") do
+testset("failures") do
   @test true
-  test("can come from nested tests") do
+  testset("can come from nested tests") do
     @test true
     @test false
   end
 end
 
 @test a == 1
-a = 1
